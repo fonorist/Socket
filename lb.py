c6cb387e21ea23129702b60910295114e50d11b9
fonorist <39277018+fonorist@users.noreply.github.com>
2021-06-07 20:49:25 +0300

    Update lb.py

--- lb.py
+++ lb.py
@@ -3,9 +3,12 @@ import socket
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as client_socket:
-        client_socket.connect(('192.168.0.102', 80))
-        message_to_send = 'M1'
-        client_socket.send(message_to_send.encode())
+        try:
+            client_socket.connect(('192.168.0.102', 80))
+            message_to_send = 'M1'
+            client_socket.send(message_to_send.encode())
+        except Exception as error:
+            print(error)
 
 
 if __name__ == 'main':
