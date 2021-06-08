0a0cab47479434cfb87ca3c2967740079092c6e0
fonorist <39277018+fonorist@users.noreply.github.com>
2021-06-08 12:57:51 +0300

    Update lb.py

--- lb.py
+++ lb.py
@@ -1,36 +1,32 @@
-import os
 import socket
-import sys
 
 
 def main():
-    print("dssdfsf")
-    try:
-        with socket.socket() as server_socket:
-            server_socket.bind(('10.0.0.1', 80))
-            server_socket.listen(10)
-            connection_socket, connection_address = server_socket.accept()
-            print(connection_socket)
-            print(connection_address)
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as client_socket1:
+    print("Starting")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as client_socket1:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as client_socket2:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as client_socket3:
                 client_socket1.connect(('192.168.0.101', 80))
-                message = connection_socket.recv(1024)
-                print(message)
-                client_socket1.send(message)
-                answer = client_socket1.recv(1024)
-                print(answer)
-                connection_socket.send(answer)
-                client_socket1.close()
-            connection_socket.close()
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        print(exc_type, fname, exc_tb.tb_lineno)
-
-    print('ebana rot')
+                client_socket2.connect(('192.168.0.102', 80))
+                client_socket3.connect(('192.168.0.103', 80))
+                with socket.socket() as server_socket:
+                    server_socket.bind(('10.0.0.1', 80))
+                    server_socket.listen(10)
+                    while True:
+                        connection_socket, connection_address = server_socket.accept()
+                        print(connection_socket)
+                        print(connection_address)
+                        message = connection_socket.recv(1024)
+                        print(message)
+                        client_socket1.send(message)
+                        answer = client_socket1.recv(1024)
+                        print(answer)
+                        connection_socket.send(answer)
+                        connection_socket.close()
 
 
 if __name__ == '__main__':
     main()
+
 
 
